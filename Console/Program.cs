8303689c6e6a3822fcea24b9a64c0768ba3d8975
MillerKV <MillerKV@402D-5>
2024-05-20 14:04:57 +0700

    Добавьте файлы проекта.

---
+++ Console/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using ServersLibrary;
+
+
+
+Servers servers = Servers.Instance;
+
+
+Console.WriteLine(servers.AddServer("http://example.com"));  // true
+Console.WriteLine(servers.AddServer("https://example.org")); // true
+Console.WriteLine(servers.AddServer("ftp://example.net"));   // false
+
+
+var httpServers = servers.GetHttpServers();
+var httpsServers = servers.GetHttpsServers();
+
+Console.WriteLine("HTTP servers:");
+foreach (var server in httpServers)
+{
+    Console.WriteLine(server);
+}
+
+Console.WriteLine("HTTPS servers:");
+foreach (var server in httpsServers)
+{
+    Console.WriteLine(server);
+}
+    
---
+++ ServersLibrary/Servers.cs
@@ -0,0 +1,76 @@
+﻿namespace ServersLibrary
+{
+    public sealed class Servers
+    {
+        private static readonly Servers instance = new Servers();
+        private readonly List<string> serverList = new List<string>();
+        private readonly object lockObject = new object();
+
+        private Servers() { }
+
+        public static Servers Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public bool AddServer(string server)
+        {
+            if (string.IsNullOrWhiteSpace(server) || !server.StartsWith("http://") && !server.StartsWith("https://"))
+            {
+                return false;
+            }
+
+            lock (lockObject)
+            {
+                if (serverList.Contains(server))
+                {
+                    return false;
+                }
+
+                serverList.Add(server);
+            }
+
+            return true;
+        }
+
+        public List<string> GetHttpServers()
+        {
+            var httpServers = new List<string>();
+
+            lock (lockObject)
+            {
+                foreach (var server in serverList)
+                {
+                    if (server.StartsWith("http://"))
+                    {
+                        httpServers.Add(server);
+                    }
+                }
+            }
+
+            return httpServers;
+        }
+
+        public List<string> GetHttpsServers()
+        {
+            var httpsServers = new List<string>();
+
+            lock (lockObject)
+            {
+                foreach (var server in serverList)
+                {
+                    if (server.StartsWith("https://"))
+                    {
+                        httpsServers.Add(server);
+                    }
+                }
+            }
+
+            return httpsServers;
+        }
+    }
+
+}
